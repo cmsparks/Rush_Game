4db9032350f965b58dcaec24c3ac6cb3b4783533
Michael Klyachman <m.k.klyachman@gmail.com>
2017-05-27 17:04:22 -0500

    more functions

--- analytics/parsing.py
+++ analytics/parsing.py
@@ -1,13 +1,13 @@
 import json
 from pprint import pprint
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+
 data_file = open('pseudo_data/players.json')
 data = json.load(data_file)
 data = data["data"]
-
 user_number = len(data)
-
+numquestions=10
 user_names = []
 for i in range(0,user_number):
 	user_names.append(data[i]["name"])
@@ -18,21 +18,54 @@ def function(n):
 	if n==False:
 		return 0
 
-def rightorwrong(user_place):
+def userplot(user_place):
+	"""Plots the results of a single user. user_place is a number which is their place in the array"""
 	name=str(data[user_place]["name"])
+
 	index=data[user_place]["current_index"]
 	responselist=data[user_place]["responses"]
-
 	responselist=list(map(function, responselist))
+
 	x_list = []
 	for i in range(1, index+1):
 		x_list.append(i)
 
 	plt.scatter(x_list, responselist)
-	plt.plot(x_list, responselist, 'r-')
-	
+	plt.plot(x_list, responselist, 'r-', label='{a}s score'.format(a=name))
 	plt.show()
 
+def wholeclassplot():
+	"""Plots the results of the whole class"""
+	for user_place in range(0,user_number):
+		name=str(data[user_place]["name"])
+		index=data[user_place]["current_index"]
+		responselist=data[user_place]["responses"]
+		responselist=list(map(function, responselist))
+		x_list = []
+		for i in range(1, index+1):
+			x_list.append(i)
+		plt.scatter(x_list, responselist)
+		plt.plot(x_list, responselist)
+	plt.show()
 
+def averagesclass():
+	listofresults=[]
+	for i in range(0,user_number):
+		current=list(map(function, data[i]["responses"]))
+		a=len(current)
+		for i in range(0,numquestions-a):
+			current.append(0)
 
-rightorwrong(1)
+		listofresults.append(current)
+
+	print(listofresults)
+
+	results= map(sum, zip(*listofresults))
+	print(results)
+	x_values=[]
+	for i in range(1,numquestions+1):
+		x_values.append(i)
+
+	plt.scatter(x_values, results)
+	plt.plot(x_values, results)
+	plt.show()
