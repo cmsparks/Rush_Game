ee226da7c23c74c48fe4690e9cf0afe3980e3172
jsmargolin <jeremy.margolin@gmail.com>
2017-05-27 18:43:40 -0500

    ashfi

--- build/public/teacher/progress/main.js
+++ build/public/teacher/progress/main.js
@@ -1,6 +1,33 @@
-function myFunction() {
-    var node = document.createElement("IMG");
-    for (i = 0; )
-    var t = document.createTextNode("This is a paragraph.");
-    node.appendChild(t);
-    document.getElementById("userbar").appendChild(node);
+//socket = io();
+userpos = [];
+for (i = 0; i < data.length; i++) {
+	userpos.push(3);
+}
+function getPos(uid) {
+	for (i = 0; i < data.length; i++) {
+		if (data[i].uid == uid) {
+			return i;
+		}
+	}
+	return -1;
+}
+function update(uid) {
+	userpos[getPos(uid)] += 1;
+}
+//socket.get('result', update(uid));
+function create_users() {
+	for (i = 0; i < data.length; i++) {
+		var node = document.createElement("P");
+    	var textnode = document.createTextNode(data[i].name);
+    	node.appendChild(textnode);
+    	document.getElementById("userbar").appendChild(node);
+    	for (k = 0; k < userpos[i]; k++) {
+    		var img = document.createElement("img");
+			img.src = "#";
+			document.getElementById("userbar").appendChild(img);
+    	}
+    	var img = document.createElement("img");
+		img.src = "#";
+		document.getElementById("userbar").appendChild(img);
+    }
+}
