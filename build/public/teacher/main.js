af499af1da4128e017a7f7af1c2c1b1e2fec0e28
jsmargolin <jeremy.margolin@gmail.com>
2017-05-27 16:20:23 -0500

    asyghf

---
+++ build/public/teacher/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var node = document.createElement("IMG");
+    for (i = 0; )
+    var t = document.createTextNode("This is a paragraph.");
+    node.appendChild(t);
+    document.getElementById("userbar").appendChild(node);
