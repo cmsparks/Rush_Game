2c0d11b56f28fc476a6c76a76c7c905e067fc6f7
mmearly <matthew.ergc.lin.wy@gmail.com>
2017-05-27 15:46:23 -0500

    Basic question creator stuff

---
+++ QuestionCreator/questionCreatorJS.js
@@ -0,0 +1,72 @@
+var count = 2;
+function newQues(){
+	
+	//document.getElementById("1ans1").value = "Fifth Avenue, New York City";
+
+	alert("Running the correct version!");
+
+	var challenge = document.createElement("input");
+    challenge.name="challenge"+count;
+    //var qText = "Question "+count;
+
+    var questNumArea = document.createElement("p");//
+    var oneNum = document.createElement("p");
+    var twoNum = document.createElement("p");
+    var threeNum = document.createElement("p");
+    var fourNum = document.createElement("p");
+    var br = document.createElement("br");
+    var questNumText = document.createTextNode("Question "+count+":");//
+    var oneText = document.createTextNode("Answer 1:");
+    var twoText = document.createTextNode("Answer 2:");
+    var threeText = document.createTextNode("Answer 3:");
+    var fourText = document.createTextNode("Answer 4:");
+    questNumArea.appendChild(questNumText);
+    oneNum.appendChild(oneText);
+    twoNum.appendChild(twoText);
+    threeNum.appendChild(threeText);
+    fourNum.appendChild(fourText);
+
+	var ansOne = document.createElement("input");
+	ansOne.name = count+"ans"+1;
+	ansOne.value = "This is a test";
+
+	var ansTwo = document.createElement("input");
+	ansTwo.name = count+"ans"+2;
+
+	var ansThree = document.createElement("input");
+	ansThree.name = count+"ans"+3;
+
+	var ansFour = document.createElement("input");
+	ansFour.name = count+"ans"+4;
+	var formPlate = document.getElementById("form");
+	
+
+	formPlate.appendChild(questNumArea);//
+	formPlate.appendChild(challenge);
+	//formPlate.appendChild(br);
+	formPlate.appendChild(oneNum);
+	formPlate.appendChild(ansOne);
+	formPlate.appendChild(twoNum);
+	formPlate.appendChild(ansTwo);
+	//formPlate.appendChild(br);
+	formPlate.appendChild(threeNum);
+	formPlate.appendChild(ansThree);
+	formPlate.appendChild(fourNum);
+	formPlate.appendChild(ansFour);
+	formPlate.appendChild(br);
+
+	var element = document.getElementById("newQuestionHere");
+	element.appendChild(formPlate);
+
+	
+	document.body.innerHTML +=" ";
+
+	//document.getElementById("newQuestionHere").innerHTML = "what does this do";-->
+	count++;
+}
+
+function finish(){
+	alert("Done");
+	//var name = Document.-->
+
+}
