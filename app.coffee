7d1172a24bb6929d162e4c61c0f8a8446d39e03e
Jetmate <antonoutkine@gmail.com>
2017-05-27 15:41:56 -0500

    adsf

--- app.coffee
+++ app.coffee
@@ -7,7 +7,22 @@ app = express()
 server = app.listen process.env.PORT or 3000
 io = require('socket.io') server
 
+questions = undefined
+players = []
+stage = 'questions'
 
 app.use helmet()
 app.use compression()
+app.get '/', (req, res, next) ->
+  if stage == 'game'
+    res.redirect('/game/')
+  else
+    next()
 app.use express.static 'build/public'
+
+io.on 'connection', (socket) ->
+  socket.on 'questions', (questions_) ->
+    questions = questions_
+    stage = 'players'
+  socket.on 'player', (player) ->
+    players.push(player)
--- build/app.js
+++ build/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, compression, express, helmet, http, io, server;
+  var app, compression, express, helmet, http, io, players, questions, server, stage;
 
   helmet = require('helmet');
 
@@ -15,10 +15,33 @@
 
   io = require('socket.io')(server);
 
+  questions = void 0;
+
+  players = void 0;
+
+  stage = 'prep';
+
   app.use(helmet());
 
   app.use(compression());
 
+  app.get('/', function(req, res, next) {
+    if (stage === 'game') {
+      return res.redirect('/game/');
+    } else {
+      return next();
+    }
+  });
+
   app.use(express["static"]('build/public'));
+
+  io.on('connection', function(socket) {
+    socket.on('questions', function(questions_) {
+      return questions = questions_;
+    });
+    return socket.on('players', function(players_) {
+      return players = players_;
+    });
+  });
 
 }).call(this);
