768de0d686526e7c834a17c4eaf19bccda91dd04
cmsparks <Christian@Christians-MacBook-Air.local>
2017-05-28 01:53:22 -0500

    asdfasdfaSD

--- build/public/teacher/analytics/Players/analytics.js
+++ build/public/teacher/analytics/Players/analytics.js
@@ -4,11 +4,3 @@ function init() {
 function rdirPlayers() {
 	window.location.replace("../Class/analytics.html")
 }
-
-function divgenerate(playername) {
-	var div=document.createElement("Div");
-	
-	var t= document.createTextNode("Text");
-	div.appendChild(t);
-	document.body.appendChild(div);
-}
