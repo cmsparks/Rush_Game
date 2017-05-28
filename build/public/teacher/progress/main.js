bb71f04bfeaf26d8f4e421cbd83e9f1985f5cf38
jsmargolin <jeremy.margolin@gmail.com>
2017-05-27 19:09:16 -0500

    gbfhs

--- build/public/teacher/progress/main.js
+++ build/public/teacher/progress/main.js
@@ -1,4 +1,4 @@
-//socket = io();
+socket = io();
 userpos = [];
 for (i = 0; i < data.length; i++) {
 	userpos.push(3);
@@ -14,7 +14,7 @@ function getPos(uid) {
 function update(uid) {
 	userpos[getPos(uid)] += 1;
 }
-//socket.get('result', update(uid));
+socket.get('result', update(uid));
 function create_users() {
 	for (i = 0; i < data.length; i++) {
 		var node = document.createElement("P");
